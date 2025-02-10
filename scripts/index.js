150970b66f2bc170283f074fc49c70601e58b160
FaridaNelson <faridanelson@gmail.com>
2025-02-10 11:11:35 -0800

    added JavaScript file with cards to the project

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "New York Library",
+    link: "https://unsplash.com/photos/a-black-and-white-photo-of-a-city-street-2LBEiScKJs0",
+  },
+  {
+    name: "A Beach in the Phillipines",
+    link: "https://unsplash.com/photos/aerial-view-of-beach-during-daytime-Ad7-qVkGy7Y",
+  },
+  {
+    name: "A Landscape In Canada",
+    link: "https://unsplash.com/photos/a-scenic-view-of-a-mountain-range-with-a-lake-in-the-foreground-j0qFdnSC_bY",
+  },
+  {
+    name: "Pfeiffer Beach",
+    link: "https://unsplash.com/photos/a-large-rock-formation-with-a-cave-in-the-middle-of-it-dlBxLYUTZQc",
+  },
+  {
+    name: "Idyllic Alpine Waterscape",
+    link: "https://unsplash.com/photos/a-small-town-on-a-lake-surrounded-by-mountains-SA5RjUtCSkI",
+  },
+  {
+    name: "Oia sunset in Santorini, Greece",
+    link: "https://unsplash.com/photos/a-view-of-a-white-village-on-a-cliff-TRyUyUfm_wU",
+  },
+];
