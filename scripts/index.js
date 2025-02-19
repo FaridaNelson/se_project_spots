8088020767c0101491188b9e5c7e8632fa9ff805
FaridaNelson <faridanelson@gmail.com>
2025-02-18 23:49:18 -0800

    added template for the cards and connected all attributes

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
     name: "New York Library",
-    link: "https://unsplash.com/photos/a-black-and-white-photo-of-a-city-street-2LBEiScKJs0",
+    link: "https://images.unsplash.com/photo-1737923336697-621a80820522?q=80&w=2862&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "A Beach in the Phillipines",
-    link: "https://unsplash.com/photos/aerial-view-of-beach-during-daytime-Ad7-qVkGy7Y",
+    link: "https://images.unsplash.com/photo-1586768798120-95597acaa6e3?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "A Landscape In Canada",
-    link: "https://unsplash.com/photos/a-scenic-view-of-a-mountain-range-with-a-lake-in-the-foreground-j0qFdnSC_bY",
+    link: "https://images.unsplash.com/photo-1738936339590-ea1fc8bd9732?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Pfeiffer Beach",
-    link: "https://unsplash.com/photos/a-large-rock-formation-with-a-cave-in-the-middle-of-it-dlBxLYUTZQc",
+    link: "https://images.unsplash.com/photo-1739057736231-3577bfc1a1b9?q=80&w=2825&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Idyllic Alpine Waterscape",
-    link: "https://unsplash.com/photos/a-small-town-on-a-lake-surrounded-by-mountains-SA5RjUtCSkI",
+    link: "https://images.unsplash.com/photo-1738599778390-af77d7cf10e6?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Oia sunset in Santorini, Greece",
-    link: "https://unsplash.com/photos/a-view-of-a-white-village-on-a-cliff-TRyUyUfm_wU",
+    link: "https://images.unsplash.com/photo-1569581464548-66d245ae5ca5?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
@@ -41,6 +41,25 @@ document.addEventListener("DOMContentLoaded", function () {
   const modalProfileDescription = editProfileModal.querySelector(
     "#profile-description-input"
   );
+
+  const cardTemplate = document.querySelector("#card-template");
+  const cardsList = document.querySelector(".cards__list");
+
+  function getCardElement(data) {
+    console.log(data);
+    const cardElement = cardTemplate.content
+      .querySelector(".card")
+      .cloneNode(true);
+
+    const cardNameElement = cardElement.querySelector(".card__title");
+    const cardImage = cardElement.querySelector(".card__image");
+
+    cardNameElement.textContent = data.name;
+    cardImage.src = data.link;
+    cardImage.alt = `Image of ${data.name}`;
+
+    return cardElement;
+  }
 
   function openModal() {
     modalProfileName.value = profileName.textContent;
@@ -62,4 +81,9 @@ document.addEventListener("DOMContentLoaded", function () {
   profileEditButton.addEventListener("click", openModal);
   modalCloseButton.addEventListener("click", closeModal);
   modalFormElement.addEventListener("submit", handleModalFormSubmit);
+
+  for (let i = 0; i < initialCards.length; i++) {
+    const cardElement = getCardElement(initialCards[i]);
+    cardsList.prepend(cardElement);
+  }
 });
